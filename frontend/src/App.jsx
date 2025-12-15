972bcdbdd9cc0f560cd4337e89b3286058eb6b1b
Abiola-Ajayi Kehinde <kennyabiolaajayi@gmail.com>
2025-12-15 12:36:06 +0100

    frontend-artialy  linked to backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,15 +11,30 @@ import { Routes, Route, Navigate } from "react-router";
 import HomePage from "./pages/HomePage";
 import ProblemsPage from "./pages/ProblemsPage";
 import { Toaster } from "react-hot-toast";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
-  const { isSignedIn } = useUser();
+  const { isSignedIn, isLoaded } = useUser();
+
+  if (!isLoaded) return null;
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/"
+          element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />}
+        />
+        <Route
+          path="/dashboard"
+          element={isSignedIn ? <Dashboard /> : <Navigate to={"/"} />}
+          // element={<Dashboard />}
+        />
         <Route
           path="/problems"
+          element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />}
+        />
+        <Route
+          path="/problem/:id"
           element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />}
         />
       </Routes>
